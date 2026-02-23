c7b16243cdd6dc519dbaccae971223b04c9d017f
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-02-24 01:55:14 +0530

    feat: add user authentication dependencies, environment config import, and users table migration

---
+++ src/main/resources/db/migration/V4__create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users
+(
+    id         BINARY(16) PRIMARY KEY,
+    username   VARCHAR(50) UNIQUE  NOT NULL,
+    email      VARCHAR(100) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    full_name  VARCHAR(100),
+    role       VARCHAR(20) DEFAULT 'CASHIER',
+    created_at TIMESTAMP   DEFAULT CURRENT_TIMESTAMP
+);
